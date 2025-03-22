6e75f85d0ad4a82a279f003e6f5649de7e480fe0
niallbarrett <niall.barrett7@gmail.com>
2025-03-22 23:47:18 +0000

    Sort dates chronologically

--- src/components/Cullendar/components/Day.vue
+++ src/components/Cullendar/components/Day.vue
@@ -1,5 +1,5 @@
 <template>
-  <slot v-bind="{ api, events }"/>
+  <slot v-bind="{ api, events: chronological }"/>
 </template>
 
 <script setup>
@@ -27,4 +27,5 @@ const props = defineProps({
 const { utils } = toRefs(props.api)
 
 const events = computed(() => utils.value.getEvents(props.resource.id, props.date))
+const chronological = computed(() => Array.from(events.value.values()).sort((a, b) => Date.parse(a.start) - Date.parse(b.start)))
 </script>
