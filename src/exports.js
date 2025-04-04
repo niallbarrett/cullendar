2b4c87c5d3fdb33d75dec988b5bfcba1c50ec29e
niallbarrett <niall.barrett7@gmail.com>
2025-04-04 13:31:14 +0100

    Export default component

--- src/exports.js
+++ src/exports.js
@@ -1,7 +1,13 @@
 import Cullendar from '@/components/Cullendar'
+import DragEvent from '@/components/Cullendar/components/DragEvent'
+import DropDay from '@/components/Cullendar/components/DropDay'
 import create from '@/components/Cullendar/api'
+
+export default { install: app => app.component('Cullendar', Cullendar) }
 
 export {
   Cullendar,
+  DragEvent,
+  DropDay,
   create
 }
