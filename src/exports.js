44a13241c31f9cc456cdd18785fa1bc5b51b8add
Niall Barrett <niall.barrett7@gmail.com>
2025-04-07 13:43:17 +0100

    Export ResizeHandle

--- src/exports.js
+++ src/exports.js
@@ -1,7 +1,10 @@
+// API
+import create from '@/components/Cullendar/api'
+// Components
 import Cullendar from '@/components/Cullendar'
 import DragEvent from '@/components/Cullendar/components/DragEvent'
 import DropDay from '@/components/Cullendar/components/DropDay'
-import create from '@/components/Cullendar/api'
+import ResizeHandle from '@/components/Cullendar/components/ResizeHandle'
 
 export default { install: app => app.component('Cullendar', Cullendar) }
 
@@ -9,5 +12,6 @@ export {
   Cullendar,
   DragEvent,
   DropDay,
+  ResizeHandle,
   create
 }
