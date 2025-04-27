540c57719a01265faf6b8f38f3063fc57d35ea65
niallbarrett <niall.barrett7@gmail.com>
2025-04-27 16:25:07 +0100

    Build the d.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 // Plugins
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), dts({
+    tsconfigPath: './tsconfig.app.json',
+    rollupTypes: true
+  })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
